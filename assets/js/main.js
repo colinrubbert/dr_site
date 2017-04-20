4871dcf7a2c1010e83a3b6a531b22ed1f0ebe798
Colin Rubbert <colinrubbert@gmail.com>
2017-04-20 10:19:24 -0500

    Work on the contact section and add employee photos

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,19 @@ var mobileMenu = document.getElementById("mobile-navigation-wrapper").classList;
 function toggleMenu() {
   mobileMenu.toggle("open");
 }
+
+// Function to display Google Maps location
+function initMap() {
+  var myLatLng = {lat: 44.025341, lng: -88.537153};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Dr. Kirtida N. Ringwala M.D.'
+  });
+}
